099d725845e85b6e63ba343b8302284639bdb557
srajaben@gmail.com <srajabenQgmail.com>
2024-10-13 21:52:48 -0400

    Added java files

---
+++ src/test/java/com/ebs/ebs_spring_boot_cicd/EbsSpringBootCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebs.ebs_spring_boot_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbsSpringBootCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
